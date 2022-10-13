e1bc4e3f7f604f4f9c52c14dce5eda82dafa5870
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-13 09:03:57 +0500

    Task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,8 +5,9 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-result(n);
-void result(int n)
+Result(n);
+
+void Result(int n)
 {
     int res = 0;
     while(n/10 != 0)
